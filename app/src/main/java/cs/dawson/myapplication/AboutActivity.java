a70e7b48e0ff584fee621a5764ec5c578a115d2c
Hannah Ly <lnh.hannah@gmail.com>
2017-10-03 22:55:40 -0400

    Changed the 2 buttons' names in activity_main.xml to Hint and About. Added a method in MainActiviy.java, aboutGameInfo, for it to call a new activity, AboutActivity, to display the About Game page. Tested it and was about to pop the About page at the start of the quiz and in the middle of the quiz without messing up the presented question.

--- app/src/main/java/cs/dawson/myapplication/AboutActivity.java
+++ app/src/main/java/cs/dawson/myapplication/AboutActivity.java
@@ -1,8 +1,16 @@
 package cs.dawson.myapplication;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
- * Created by Cookiebutt on 03/10/2017.
+ *
  */
 
-public class AboutActivity {
+public class AboutActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
 }
